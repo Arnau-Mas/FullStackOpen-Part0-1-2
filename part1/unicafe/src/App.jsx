f6186dd4d31e4d014e9472de8e959248fe7548a3
Arnau-Mas <arnau.mava@gmail.com>
2025-03-12 12:22:08 +0100

    exercise 1.11 finished and refactored

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
-/* const StatisticLine = ({text, value}) => <p>{text}: {value}</p> */
+const StatisticLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const  Statistics = ({good, neutral, bad, all}) => {
   const average = all===0 ? 0 : (good-bad)/all;
@@ -20,44 +20,14 @@ const  Statistics = ({good, neutral, bad, all}) => {
     <h1>Statistics</h1>
     <table>
       <tbody>
-        <tr>
-          <td>good</td>
-          <td>{good}</td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td>{neutral}</td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td>{bad}</td>
-        </tr>
-        <tr>
-          <td>all</td>
-          <td>{all}</td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td>{average}</td>
-        </tr>
-        <tr>
-          <td>positive</td>
-          <td>{positive}</td>
-        </tr>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} /> 
       </tbody>
     </table>
-      {/*    
-
-      ****content of 1.10 exercise****
-
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} /> 
-      
-      */}
     </div>
   )
 }
