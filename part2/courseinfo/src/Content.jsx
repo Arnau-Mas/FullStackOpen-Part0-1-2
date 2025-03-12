190532201ae7ca1c327f61e4e0dcb555c52b980d
Arnau-Mas <arnau.mava@gmail.com>
2025-03-12 17:00:55 +0100

    exercise 2.1 finished

---
+++ part2/courseinfo/src/Content.jsx
@@ -0,0 +1,9 @@
+import { Part } from "./Part"
+
+export const Content = ({parts}) => {
+  return (
+    <>
+        {parts.map(part => <Part part={part.name} exercise={part.exercises}/>)}
+    </>
+  )
+}
