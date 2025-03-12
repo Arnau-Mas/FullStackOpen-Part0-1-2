a2300d6cf6ecb9c24c513b5a0f147993a36a24d9
Arnau-Mas <arnau.mava@gmail.com>
2025-03-12 16:42:34 +0100

    exercise 2.1 finished

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -1,11 +1,9 @@
 import { Part } from "./Part"
 
-export const Content = ({parts:[part1, part2, part3]}) => {
+export const Content = ({parts}) => {
   return (
     <>
-        <Part part={part1.name} exercise={part1.exercises}/>
-        <Part part={part2.name} exercise={part2.exercises}/>
-        <Part part={part3.name} exercise={part3.exercises}/>
+      {parts.map(part => <Part part={part.name} exercise={part.exercises}/>)}
     </>
   )
 }
