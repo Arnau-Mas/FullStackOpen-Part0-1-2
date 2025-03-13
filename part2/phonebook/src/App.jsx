f42dc48a159d302ef895ebe222de03828a64cec6
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2025-03-13 21:59:11 +0100

    exercise 2.9 finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 
   // I could do only one state to setPerson
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  
+  const [filter, setFilter] = useState("");
+
+  let filterPersons = filter ? persons.filter(person => person.name.toUpperCase().startsWith(filter.toUpperCase())) : persons;
 
   function handleName(e){
     setNewName(e.target.value)
@@ -30,9 +37,16 @@ const App = () => {
     }
   }
 
+  function handleFilter(e){
+      setFilter(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilter} type="text" name="" id="" value={filter} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleName} type='text' value={newName} />
@@ -46,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+        filterPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
       }
     </div>
   )
